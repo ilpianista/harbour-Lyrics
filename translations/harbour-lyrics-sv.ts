a5ce4b172644ca614ce8128e167cb013b74b15c1
Andrea Scarpino <me@andreascarpino.it>
2015-09-12 19:44:21 +0200

    Add Swedish translation

---
+++ translations/harbour-lyrics-sv.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="sv" version="2.1">
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="64"/>
+        <source>Clear</source>
+        <translation>Rensa</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="58"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="151"/>
+        <source>Powered by %1</source>
+        <translation>Drivs av %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="95"/>
+        <source>Artist</source>
+        <translation>Artist</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="104"/>
+        <source>Song</source>
+        <translation>Låt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="118"/>
+        <source>Search</source>
+        <translation>Sök</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="36"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="42"/>
+        <source>Provider</source>
+        <translation>Leverantör</translation>
+    </message>
+</context>
+</TS>
