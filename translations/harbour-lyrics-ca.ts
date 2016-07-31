414ac6e7a544d24a8d7ba0636f76e5c02b998962
Andrea Scarpino <me@andreascarpino.it>
2016-07-31 20:50:02 +0200

    Preview song text in the cover

--- translations/harbour-lyrics-ca.ts
+++ translations/harbour-lyrics-ca.ts
@@ -4,37 +4,37 @@
 <context>
     <name>MainPage</name>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="65"/>
+        <location filename="../qml/pages/MainPage.qml" line="76"/>
         <source>Clear</source>
         <translation>Neteja</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="59"/>
+        <location filename="../qml/pages/MainPage.qml" line="70"/>
         <source>Settings</source>
         <translation>Configuració</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="165"/>
+        <location filename="../qml/pages/MainPage.qml" line="179"/>
         <source>Powered by %1</source>
         <translation>Impulsat per %1</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="108"/>
+        <location filename="../qml/pages/MainPage.qml" line="119"/>
         <source>Artist</source>
         <translation>Artista</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="78"/>
+        <location filename="../qml/pages/MainPage.qml" line="89"/>
         <source>Copy to clipboard</source>
         <translation>Copia al porta-retalls</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="119"/>
+        <location filename="../qml/pages/MainPage.qml" line="131"/>
         <source>Song</source>
         <translation>Cançó</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="135"/>
+        <location filename="../qml/pages/MainPage.qml" line="148"/>
         <source>Search</source>
         <translation>Cerca</translation>
     </message>
@@ -52,7 +52,17 @@
         <translation>Proveïdor</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="64"/>
+        <location filename="../qml/pages/Settings.qml" line="66"/>
+        <source>Enable Media Player scanner</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="67"/>
+        <source>Checks Media Player to get song info. Do not focus any field to allow text substitution.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="76"/>
         <source>Clear cache</source>
         <translation>Neteja la memòria cau</translation>
     </message>
