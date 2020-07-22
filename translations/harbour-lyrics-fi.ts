c8c4197130e491a3a933023fd224ef1c8a409834
J. Lavoie <j.lavoie@net-c.ca>
2020-07-22 15:27:19 +0000

    Translated using Weblate (Finnish)
    Currently translated at 83.3% (10 of 12 strings)
    
    Translation: Harbour Lyrics/Translations
    Translate-URL: https://hosted.weblate.org/projects/harbour-lyrics/translations/fi/

--- translations/harbour-lyrics-fi.ts
+++ translations/harbour-lyrics-fi.ts
@@ -1,4 +1,6 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="fi" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS language="fi" version="2.1">
 <context>
     <name>MainPage</name>
     <message>
@@ -14,7 +16,7 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="179"/>
         <source>Powered by %1</source>
-        <translation>Powered by %1</translation>
+        <translation>Tuen toimitti %1</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="119"/>
@@ -24,7 +26,7 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="89"/>
         <source>Copy to clipboard</source>
-        <translation type="unfinished"/>
+        <translation>Kopioi leikepöydälle</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="131"/>
@@ -52,17 +54,17 @@
     <message>
         <location filename="../qml/pages/Settings.qml" line="66"/>
         <source>Enable Media Player scanner</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="67"/>
         <source>Checks Media Player to get song info. Do not focus any field to allow text substitution.</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="76"/>
         <source>Clear cache</source>
-        <translation type="unfinished"/>
+        <translation>Tyhjennä välimuisti</translation>
     </message>
 </context>
 </TS>
