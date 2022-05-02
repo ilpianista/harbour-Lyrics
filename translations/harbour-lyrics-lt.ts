54b5d287e03a9248000b74e848976009167dc4da
Weblate <hosted@weblate.org>
2022-05-02 19:55:41 +0000

    Translations update from Weblate

---
+++ translations/harbour-lyrics-lt.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lt">
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="76"/>
+        <source>Clear</source>
+        <translation>Išvalyti</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="70"/>
+        <source>Settings</source>
+        <translation>Nustatymai</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="180"/>
+        <source>Powered by %1</source>
+        <translation>Paremta %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="120"/>
+        <source>Artist</source>
+        <translation>Atlikėjas</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="89"/>
+        <source>Copy to clipboard</source>
+        <translation>Kopijuoti į iškarpinę</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="132"/>
+        <source>Song</source>
+        <translation>Daina</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="149"/>
+        <source>Search</source>
+        <translation>Ieškoti</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="39"/>
+        <source>Settings</source>
+        <translation>Nustatymai</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="45"/>
+        <source>Provider</source>
+        <translation>Šaltinis</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="66"/>
+        <source>Enable Media Player scanner</source>
+        <translation>Įgalinti Media Player skaitytuvą</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="67"/>
+        <source>Checks Media Player to get song info. Do not focus any field to allow text substitution.</source>
+        <translation>Patikrina Media Player, kad gautų informaciją apie dainą. Nežymėkite jokio lauko, kad būtų galima prireikus pakeisti tekstą.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="76"/>
+        <source>Clear cache</source>
+        <translation>Išvalyti talpyklą</translation>
+    </message>
+</context>
+</TS>
