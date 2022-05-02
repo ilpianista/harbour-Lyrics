e425f653ce66808d5d9db0b9a9f7b61c9623190d
Andrea Scarpino <andrea@scarpino.dev>
2022-05-02 23:05:22 +0200

    Release 0.2.0

--- translations/harbour-lyrics-lt.ts
+++ translations/harbour-lyrics-lt.ts
@@ -4,37 +4,37 @@
 <context>
     <name>MainPage</name>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="76"/>
+        <location filename="../qml/pages/MainPage.qml" line="77"/>
         <source>Clear</source>
         <translation>Išvalyti</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="70"/>
+        <location filename="../qml/pages/MainPage.qml" line="71"/>
         <source>Settings</source>
         <translation>Nustatymai</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="180"/>
+        <location filename="../qml/pages/MainPage.qml" line="193"/>
         <source>Powered by %1</source>
         <translation>Paremta %1</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="120"/>
+        <location filename="../qml/pages/MainPage.qml" line="121"/>
         <source>Artist</source>
         <translation>Atlikėjas</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="89"/>
+        <location filename="../qml/pages/MainPage.qml" line="90"/>
         <source>Copy to clipboard</source>
         <translation>Kopijuoti į iškarpinę</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="132"/>
+        <location filename="../qml/pages/MainPage.qml" line="133"/>
         <source>Song</source>
         <translation>Daina</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="149"/>
+        <location filename="../qml/pages/MainPage.qml" line="150"/>
         <source>Search</source>
         <translation>Ieškoti</translation>
     </message>
@@ -52,17 +52,17 @@
         <translation>Šaltinis</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="66"/>
+        <location filename="../qml/pages/Settings.qml" line="58"/>
         <source>Enable Media Player scanner</source>
         <translation>Įgalinti Media Player skaitytuvą</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="67"/>
+        <location filename="../qml/pages/Settings.qml" line="59"/>
         <source>Checks Media Player to get song info. Do not focus any field to allow text substitution.</source>
         <translation>Patikrina Media Player, kad gautų informaciją apie dainą. Nežymėkite jokio lauko, kad būtų galima prireikus pakeisti tekstą.</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="76"/>
+        <location filename="../qml/pages/Settings.qml" line="68"/>
         <source>Clear cache</source>
         <translation>Išvalyti talpyklą</translation>
     </message>
